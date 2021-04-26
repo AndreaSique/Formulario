bab42537c3e0c2ea2d76af05a3d7346eaa413e9d
AndreaSique <69050106+AndreaSique@users.noreply.github.com>
2021-04-26 16:39:23 -0600

    se agrego el backend de la calculadora

---
+++ js/Main.js
@@ -0,0 +1,65 @@
+function multiplicar(){
+    var num1 = document.getElementById("num-1").value
+    var num2 = document.getElementById("num-2").value
+    var txt_respuesta = document.getElementById("txt-respuesta")
+
+
+    var respuesta = num1*num2
+    txt_respuesta.textContent = respuesta
+}
+
+var btn_m = document.getElementById("btn_m")
+
+btn_m.addEventListener('click', multiplicar)
+
+
+//-----------------------------------------------------
+
+function dividir(){
+    var num1 = document.getElementById("num-1").value
+    var num2 = document.getElementById("num-2").value
+    var txt_respuesta = document.getElementById("txt-respuesta")
+
+    var respuesta = num1/num2
+    txt_respuesta.textContent = respuesta
+}
+
+var btn_d = document.getElementById("btn_d")
+btn_d.addEventListener('click', dividir)
+
+
+//----------------------------------------------------
+
+function sumar(){
+    var num1 = document.getElementById("num-1").value
+    var num2 = document.getElementById("num-2").value
+    var txt_respuesta = document.getElementById("txt-respuesta")
+
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+
+    var respuesta = num1+num2
+    txt_respuesta.textContent = respuesta
+}
+
+var btn_s = document.getElementById("btn_s")
+btn_s.addEventListener('click', sumar)
+
+
+//----------------------------------------------------
+
+function restar(){
+    var num1 = document.getElementById("num-1").value
+    var num2 = document.getElementById("num-2").value
+    var txt_respuesta = document.getElementById("txt-respuesta")
+
+    var respuesta = num1-num2
+    txt_respuesta.textContent = respuesta
+}
+
+var btn_r = document.getElementById("btn_r")
+btn_r.addEventListener('click', restar)
+
+
+
+
